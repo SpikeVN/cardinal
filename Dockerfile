036112fe2f8a02d57cbfe5c1d02a5f597e369202
SpikeBonjour <triphuongvasa@gmail.com>
2023-06-27 21:35:07 +0700

    remove wkpdf

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@
 FROM python:3.10
 WORKDIR /app
 COPY requirements.txt requirements.txt
-RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
-RUN apt install ./wkhtmltox_0.12.6.1-3.bookworm_amd64.deb
 RUN pip3 install -r requirements.txt
 COPY . .
 CMD ["python3", "main.py"]
--- modules/wfsolver/__init__.py
+++ modules/wfsolver/__init__.py
@@ -15,11 +15,9 @@
 import json
 import os
 import requests
-import io
 
 import disnake
 import i18n
-import imgkit
 
 from jinja2 import Environment, FileSystemLoader
 from disnake.ext import commands
@@ -65,15 +63,8 @@ class WFSolver(commands.Cog):
             max_search: int = commands.Param(
                 description=i18n.localized_argument_description("wfsolver", "max"),
                 default=-1
-            ),
-            experimental: bool = False
+            )
     ):
-        if experimental:
-            await interaction.response.defer()
-        # parser = wiktionaryparser.WiktionaryParser()
-        # parser.set_default_language('english')
-        # parser.include_part_of_speech(pos)
-        # words = parser.fetch('test')
         potential = {}
         for (w, pos), ds in WORDS.items():
             if pos_search != "*":
@@ -101,76 +92,43 @@ class WFSolver(commands.Cog):
         if max_search != -1:
             potential = potential[:max_search]
         await interaction.send(f"`Query OK, found {len(potential)} entries.`")
-        if not experimental:
-            index_length = len(str(len(potential)))
-            if index_length < 5:
-                index_length = 10
-            word_length = max([len(str(k)) for k in potential.keys()])
-            print(sorted(potential.keys(), key=lambda x: len(x), reverse=True))
-            if word_length < 4:
-                word_length = 9
-            pos_length = max([len(str(", ".join(v["allpos"]))) for v in potential.values()])
-            if pos_length < 14:
-                pos_length = 19
-            # Python moment
-            message = eval(
+
+        index_length = len(str(len(potential)))
+        if index_length < 5:
+            index_length = 10
+        word_length = max([len(str(k)) for k in potential.keys()])
+        print(sorted(potential.keys(), key=lambda x: len(x), reverse=True))
+        if word_length < 4:
+            word_length = 9
+        pos_length = max([len(str(", ".join(v["allpos"]))) for v in potential.values()])
+        if pos_length < 14:
+            pos_length = 19
+        # Python moment
+        message = eval(
+            "f\"" +
+            "```| {'INDEX':il} | {'WORD':wl} | {'PART OF SPEECH':pl} |\\n\""
+            .replace("il", str(index_length))
+            .replace("wl", str(word_length))
+            .replace("pl", str(pos_length))
+        )
+        message += "="*(len(message)-4) + "\n"
+        for i, (w, d) in enumerate(potential.items()):
+            ap = ", ".join([WEBSTER_WIKITONARY_MAP[p] for p in d['allpos']])
+            tmpmsg = eval(
                 "f\"" +
-                "```| {'INDEX':il} | {'WORD':wl} | {'PART OF SPEECH':pl} |\\n\""
-                .replace("il", str(index_length))
-                .replace("wl", str(word_length))
-                .replace("pl", str(pos_length))
+                "| {str(i):$$il} | {w:$$wl} | {ap:$$pl} |\\n\""
+                .replace("$$il", str(index_length))
+                .replace("$$wl", str(word_length))
+                .replace("$$pl", str(pos_length))
+                .replace("str(i)", str(i+1))
             )
-            message += "="*(len(message)-4) + "\n"
-            for i, (w, d) in enumerate(potential.items()):
-                ap = ", ".join([WEBSTER_WIKITONARY_MAP[p] for p in d['allpos']])
-                tmpmsg = eval(
-                    "f\"" +
-                    "| {str(i):$$il} | {w:$$wl} | {ap:$$pl} |\\n\""
-                    .replace("$$il", str(index_length))
-                    .replace("$$wl", str(word_length))
-                    .replace("$$pl", str(pos_length))
-                    .replace("str(i)", str(i+1))
-                )
-                if len(message + tmpmsg) >= 2000:
-                    message += "```"
-                    await interaction.channel.send(message)
-                    message = "```"
-                message += tmpmsg
-            message += "```"
-            await interaction.channel.send(message)
-        else:
-            images = []
-            for _ in range(len(potential.items()) // 5):
-                img = await template.render_async(words=list(enumerate(potential.items()))[:5])
-                img = imgkit.from_string(img, False, {
-                    'format': 'png',
-                    'crop-w': '400',
-                })
-                for index in range(5):
-                    try:
-                        potential.pop(list(potential.keys())[index])
-                    except KeyError:
-                        pass
-                images.append(img)
-
-            if len(images) <= 10:
-                await interaction.edit_original_response(
-                    files=[disnake.File(io.BytesIO(img), f"result{index}.png") for index, img in enumerate(images)]
-                )
-            else:
-                await interaction.edit_original_response(
-                    files=[disnake.File(io.BytesIO(img), f"result{index}.png") for index, img in enumerate(images[:10])]
-                )
-                for _ in range(10):
-                    images.pop()
-
-                for _ in range(len(potential.items()) // 10):
-                    await interaction.send(
-                        files=[disnake.File(io.BytesIO(img), f"result{index}.png") for index, img in
-                               enumerate(images[:10])]
-                    )
-                    for _ in range(10):
-                        images.pop()
+            if len(message + tmpmsg) >= 2000:
+                message += "```"
+                await interaction.channel.send(message)
+                message = "```"
+            message += tmpmsg
+        message += "```"
+        await interaction.channel.send(message)
 
 
 def setup(bot):
